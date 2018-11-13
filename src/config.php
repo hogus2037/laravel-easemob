11d85c74d708b1134b28fd62f489b931838db527
hogus <wugang@taopaitang.com>
2018-11-13 11:16:33 +0800

    修改配置文件

--- src/config.php
+++ src/config.php
@@ -3,13 +3,13 @@
 
 return [
     //默认域名
-    //'domain_name' => '',
+    'domain' => env('EASEMOB_DOMAIN', 'https://a1.easemob.com'),
     //企业名称
-    'org_name' => '1194180509099040',
+    'org_name' => env('EASEMOB_ORG_NAME', '#################'),
     //app_name
-    'app_name' => 'tpt-test',
+    'app_name' => env('EASEMOB_APP_NAME', 'test'),
     //客户id
-    'client_id'=> 'YXA6P76lAFODEeiQoEP6-gQLjg',
+    'client_id'=> env('EASEMOB_CLIENT_ID', '#################'),
     //客户秘钥
-    'client_secret' => 'YXA6YCh8YjLJ-Cmpqbo9wjABM54Lj2U'
+    'client_secret' => env('EASEMOB_CLIENT_SECRET', '###############')
 ];
